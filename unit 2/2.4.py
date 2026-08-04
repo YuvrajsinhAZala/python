5b5ee3e41c4f7db9094ab23c7ef1f8b7539939a1
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-04 12:38:31 +0530

    Refactor sum of digits program
    Refactor the sum of digits program to improve readability.

--- unit 2/2.4.py
+++ unit 2/2.4.py
@@ -1,3 +1,12 @@
 #Write a program to find the sum of digits of a
 #number using a while loop.
 
+num = int(input('enter number :'))
+summ=0
+
+
+while(num>0):
+    a=num%10
+    summ=summ+a
+    num=num//10
+print(summ)
