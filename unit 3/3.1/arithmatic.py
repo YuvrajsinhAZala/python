615f2870bc3ebd5a7cc6bed0288683d7b5c91093
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-12 12:52:38 +0530

    Add files via upload

---
+++ unit 3/3.1/arithmatic.py
@@ -0,0 +1,13 @@
+#write a program to create and import a userdefined module.
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
