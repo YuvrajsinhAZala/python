b45a774bbc64556c81e3bdba5b3a9186b0f57f75
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-12 13:10:25 +0530

    Add files via upload

---
+++ unit 3/3.3.py
@@ -0,0 +1,9 @@
+import math as m
+
+print('floor : ',m.floor(25.5))
+print('ceil : ',m.ceil(25.5))
+print('sqrt : ',m.sqrt(25))
+print('pow : ',m.pow(5,2))
+print('factorial : ',m.factorial(5))
+print('pow : ',m.pow(2,2))
+print('log : ',m.log10(1000))
