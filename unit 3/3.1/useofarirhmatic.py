36394bcc29ee84f0f00827112bfc27ebdb7dbf47
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-12 12:57:04 +0530

    Add files via upload

---
+++ unit 3/3.1/useofarirhmatic.py
@@ -0,0 +1,9 @@
+import arithmatic as ar
+
+a=int(input('enter value of num 1 : '))
+b=int(input('enter value of num 2 : '))
+
+print(ar.add(a,b))
+print(ar.sub(a,b))
+print(ar.mul(a,b))
+print(ar.div(a,b))
