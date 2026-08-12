2d1d745651a24827e6bb353f61d99e61028931b5
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-12 12:32:31 +0530

    Add files via upload

---
+++ unit 2/2.9.py
@@ -0,0 +1,24 @@
+#Write a program to demonstrate iterators and 
+#iterables in Python.
+
+
+one=[10,20,30,40,50]
+a=iter(one)
+
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+
+
+'''
+print(dic1)
+print('iterables')
+for i in one:
+    print(i)
+
+print('=========================')
+print('iterator')
+
+'''
