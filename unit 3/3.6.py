efe119294bc8f526bb1690921b2c87f18b8fc3a6
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-18 12:32:56 +0530

    Add files via upload

---
+++ unit 3/3.6.py
@@ -0,0 +1,27 @@
+# Write a program to perform file and directory
+#operations using os and sys modules. 
+
+import os
+import sys
+
+#print("Current Directory:", os.getcwd())
+
+#os.mkdir("yuvraj")
+
+# Create file
+#with open("yuvraj/demo.txt", "w") as f:
+#    f.write("Hello am yuvrajsinh")
+#with open("yuvraj/demo.txt", "r") as f:
+#    f.read("demo.txt")
+
+# List directory
+#print("Files:", os.listdir("yuvraj"))
+
+# Rename file
+#os.rename("yuvraj/demo.txt", "yuvraj/new.txt")
+
+# Delete file and directory
+#os.remove("yuvraj1/new.txt")
+#os.rmdir("yuvraj1")
+
+sys.exit()
