322a3a987fa7209ee0aa34cddb8958bb1df28010
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-22 17:39:22 +0530

    Implement factorial function using recursion
    Added a recursive function to calculate factorial alongside the existing Fibonacci function.

--- unit 1/1.10.py
+++ unit 1/1.10.py
@@ -8,3 +8,18 @@ def fibo(n):
     return fibo(n-1)+fibo(n-2)
 re=fibo(5)
 print(re)
+
+# Recursive function to find factorial
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+
+result = factorial(num)
+
+print("Factorial of", num, "is", result)
