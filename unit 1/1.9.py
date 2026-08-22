ecca7334058057f9e0e6960cfb27a146e73273b3
yuvrajsinhzala <agzala140173@gmail.com>
2026-08-22 17:34:44 +0530

    Add user-defined functions with various arguments

--- unit 1/1.9.py
+++ unit 1/1.9.py
@@ -1,4 +1,27 @@
 #Write a program to define and use user-defined
 #functions with different types of arguments.
 
+# User-defined functions
+
+# No argument
+def greet():
+    print("Hello")
+greet()
+
+# Positional arguments
+def add(a, b):
+    print("Addition:", a + b)
+add(10, 20)
+
+# Default argument
+def welcome(name="Student"):
+    print("Welcome", name)
+welcome()
+welcome("Yuvraj")
+
+# Keyword arguments
+def student(name, age):
+    print("Name:", name)
+    print("Age:", age)
+student(age=21, name="Yuvraj")
 
