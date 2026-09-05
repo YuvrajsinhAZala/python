8730a0a94f982d9c87c82b7266f898f79b9f95ef
yuvrajsinhzala <agzala140173@gmail.com>
2026-09-05 17:52:37 +0530

    Add files via upload

---
+++ unit 2/27.py
@@ -0,0 +1,11 @@
+# List Comprehension
+list1 = [x * 2 for x in range(1, 6)]
+print("List:", list1)
+
+# Dictionary Comprehension
+dict1 = {x: x * 2 for x in range(1, 6)}
+print("Dictionary:", dict1)
+
+# Set Comprehension
+set1 = {x * 2 for x in range(1, 6)}
+print("Set:", set1)
